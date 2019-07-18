254663d0710123b7b41b95b0b4a91f2c7e66c3fa
Elian Nasatto <elian.nasa123@gmail.com>
2019-07-18 09:25:13 -0300

    CRUD Usuarios

--- SistemaGerenciaProjetos/View/Controllers/UsuarioController.cs
+++ SistemaGerenciaProjetos/View/Controllers/UsuarioController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Model;
+using Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,55 @@ namespace View.Controllers
 {
     public class UsuarioController : Controller
     {
+        private UsuarioRepository repository;
         // GET: Usuario
+        public UsuarioController()
+        {
+            repository = new UsuarioRepository();
+        }
         public ActionResult Index()
         {
+            List<Usuario> lista = repository.ObterTodos();
+            ViewBag.Usuarios = lista;
             return View();
+        }
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public ActionResult Alterar(int id)
+        {
+            Usuario usuario = new Usuario();
+            usuario = repository.ObterPeloId(id);
+            ViewBag.Usuario = usuario;
+            return View();
+        }
+
+        public ActionResult Insert(string nome, string login, string senha)
+        {
+            Usuario usuario = new Usuario();
+            usuario.Nome = nome;
+            usuario.Login = login;
+            usuario.Senha = senha;
+            repository.Inserir(usuario);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Update(string nome, string login, string senha, int id)
+        {
+            Usuario usuario = new Usuario();
+            usuario.Id = id;
+            usuario.Nome = nome;
+            usuario.Login = login;
+            usuario.Senha = senha;
+            repository.Alterar(usuario);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Apagar(int id)
+        {
+            repository.Apagar(id);
+            return RedirectToAction("Index");
         }
     }
 }
