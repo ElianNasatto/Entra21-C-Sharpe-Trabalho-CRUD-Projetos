5d4e72c4c5173ddb7441ef431c1e70892c6057a7
Elian Nasatto <elian.nasa123@gmail.com>
2019-07-23 09:53:53 -0300

    Design Estados, Cidades (menos alterar), Clientes(menos inserir) e Categoria

--- SistemaGerenciaProjetos/View/Controllers/ClienteController.cs
+++ SistemaGerenciaProjetos/View/Controllers/ClienteController.cs
@@ -24,7 +24,7 @@ namespace View.Controllers
         public ActionResult Inserir()
         {
             CidadeRepository cidadeRepository = new CidadeRepository();
-            ViewBag.Cidades = cidadeRepository.ObterTodosCombobox();
+            ViewBag.Cidades = cidadeRepository.ObterTodos();
             return View();
         }
         public ActionResult Apagar(int id)
